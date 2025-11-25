b3b20dcdf60a96b323566c499e84f531c0c86b7b
Poseidon <18646154381@163.com>
2025-11-25 08:09:49 +0000

    feat: import head link to kernel

--- src/boot/head.s
+++ src/boot/head.s
@@ -11,7 +11,7 @@
  * the page directory will exist. The startup code will be overwritten by
  * the page directory.
  */
-.text
+.section .head, "ax"
 .globl idt,gdt,pg_dir,tmp_floppy_area
 pg_dir:
 .globl startup_32
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+global_asm!(include_str!("boot/head.s"), options(att_syntax));
+
 #[unsafe(no_mangle)]
-fn _start() -> ! {
-    #[allow(clippy::empty_loop)]
-    loop {}
+pub extern "C" fn main() {
+    todo!()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn stack_start() {
+    todo!()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn printk() {
+    todo!()
 }
