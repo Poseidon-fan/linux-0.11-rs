4a4ed4b164bff24088940bf7262398dd3d2f6110
Poseidon <18646154381@163.com>
2025-11-26 13:56:39 +0000

    chore: use lldb plugin in vscode

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,5 +5,8 @@ git config --global --add safe.directory $(pwd)
 # Install pre-commit hooks
 pre-commit install
 
+# Copy .vscode config to the top folder
+rm -rf .vscode && cp -r .devcontainer/vscode-config .vscode
+
 # Set global toolchain
 rustup default nightly-2025-01-27
