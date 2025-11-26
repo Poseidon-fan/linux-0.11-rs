d19b1c58cbac59d4c17f7a3d737dcb766b801f10
Poseidon <18646154381@163.com>
2025-11-26 04:36:25 +0000

    feat: tmp panic print use

--- src/panic.rs
+++ src/panic.rs
@@ -1,4 +1,19 @@
+use core::ptr;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let msg = b"panic!";
+    for (i, &ch) in msg.iter().enumerate() {
+        putc(ch, i);
+    }
     loop {}
+}
+
+fn putc(c: u8, pos: usize) {
+    let vga = 0xb8000 as *mut u8;
+
+    unsafe {
+        ptr::write_volatile(vga.add(pos * 2), c);
+        ptr::write_volatile(vga.add(pos * 2 + 1), 0x07);
+    }
 }
