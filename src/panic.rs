14a56ee985e92b475ac869e349e0a3d339d2d193
Poseidon <18646154381@163.com>
2026-01-08 09:24:28 +0000

    feat: change panic to write fulfill the screen

--- src/panic.rs
+++ src/panic.rs
@@ -2,18 +2,18 @@ use core::ptr;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    let msg = b"panic!";
-    for (i, &ch) in msg.iter().enumerate() {
-        putc(ch, i);
-    }
+    fill_screen(b'!');
     loop {}
 }
 
-fn putc(c: u8, pos: usize) {
+fn fill_screen(c: u8) {
     let vga = 0xb8000 as *mut u8;
+    const SCREEN_SIZE: usize = 80 * 25; // 80 columns x 25 rows
 
     unsafe {
-        ptr::write_volatile(vga.add(pos * 2), c);
-        ptr::write_volatile(vga.add(pos * 2 + 1), 0x07);
+        for pos in 0..SCREEN_SIZE {
+            ptr::write_volatile(vga.add(pos * 2), c);
+            ptr::write_volatile(vga.add(pos * 2 + 1), 0x07);
+        }
     }
 }
