2149e7bc709e604129579e49d7c35cef41828962
Poseidon <18646154381@163.com>
2026-01-20 07:31:45 +0000

    refactor: use match instead of if let in panic

--- src/panic.rs
+++ src/panic.rs
@@ -2,18 +2,21 @@ use crate::println;
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    if let Some(location) = info.location() {
-        println!(
-            "[kernel] Panicked at {}:{} {}",
-            location.file(),
-            location.line(),
-            info.message().as_str().ok_or("unknown panic msg").unwrap()
-        );
-    } else {
-        println!(
-            "[kernel] Panicked: {}",
-            info.message().as_str().ok_or("unknown panic msg").unwrap()
-        );
+    match info.location() {
+        Some(location) => {
+            println!(
+                "[kernel] Panicked at {}:{} {}",
+                location.file(),
+                location.line(),
+                info.message().as_str().ok_or("unknown panic msg").unwrap()
+            );
+        }
+        None => {
+            println!(
+                "[kernel] Panicked: {}",
+                info.message().as_str().ok_or("unknown panic msg").unwrap()
+            );
+        }
     }
     loop {}
 }
