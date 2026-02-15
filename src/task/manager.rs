a187583d55041a770da985933955347441721f3b
Poseidon <18646154381@163.com>
2026-02-15 12:14:01 +0000

    fix: lazy init TASK_MANAGER stack overflow bug

--- src/task/manager.rs
+++ src/task/manager.rs
@@ -1,5 +1,5 @@
 use core::mem::MaybeUninit;
-use core::ptr::addr_of_mut;
+use core::ptr::{addr_of_mut, write_bytes};
 
 use lazy_static::lazy_static;
 use log::debug;
@@ -180,50 +180,51 @@ lazy_static! {
         let init_task_ptr = addr_of_mut!(INIT_TASK_PAGE).cast::<TaskPage>();
         let init_task_addr = init_task_ptr as u32;
 
-        // Write the initial task 0 data.
-        init_task_ptr.write(TaskPage::new(
-            TaskControlBlock::new(
-                0, // pid = 0
-                TaskControlBlockInner {
-                    sched: TaskSchedInfo {
-                        state: TaskState::Running,
-                        counter: 15,
-                        priority: 15,
-                    },
-                    memory_space: Some(MemorySpace::new(0)), // task 0
-                    exit_code: 0,
-                    ldt: LocalDescriptorTable::new(0, 0x9f),
-                    tss: TaskStateSegment {
-                        back_link: 0,
-                        esp0: init_task_addr + PAGE_SIZE,
-                        ss0: KERNEL_DS.as_u32(),
-                        esp1: 0,
-                        ss1: 0,
-                        esp2: 0,
-                        ss2: 0,
-                        cr3: &pg_dir as *const u8 as u32,
-                        eip: 0,
-                        eflags: 0,
-                        eax: 0,
-                        ecx: 0,
-                        edx: 0,
-                        ebx: 0,
-                        esp: 0,
-                        ebp: 0,
-                        esi: 0,
-                        edi: 0,
-                        es: USER_DS.as_u32(),
-                        cs: USER_CS.as_u32(),
-                        ss: USER_DS.as_u32(),
-                        ds: USER_DS.as_u32(),
-                        fs: USER_DS.as_u32(),
-                        gs: USER_DS.as_u32(),
-                        ldt: selectors::ldt_selector(0).as_u32(),
-                        trace_bitmap: 0x8000_0000,
-                        i387: I387Struct::empty(),
-                    },
+        // Zero the whole task page first to avoid constructing a full 4KB TaskPage on stack.
+        write_bytes(init_task_ptr.cast::<u8>(), 0, PAGE_SIZE as usize);
+
+        // Then initialize only the PCB fields needed for task 0.
+        addr_of_mut!((*init_task_ptr).pcb).write(TaskControlBlock::new(
+            0, // pid = 0
+            TaskControlBlockInner {
+                sched: TaskSchedInfo {
+                    state: TaskState::Running,
+                    counter: 15,
+                    priority: 15,
                 },
-            ),
+                memory_space: Some(MemorySpace::new(0)), // task 0
+                exit_code: 0,
+                ldt: LocalDescriptorTable::new(0, 0x9f),
+                tss: TaskStateSegment {
+                    back_link: 0,
+                    esp0: init_task_addr + PAGE_SIZE,
+                    ss0: KERNEL_DS.as_u32(),
+                    esp1: 0,
+                    ss1: 0,
+                    esp2: 0,
+                    ss2: 0,
+                    cr3: &pg_dir as *const u8 as u32,
+                    eip: 0,
+                    eflags: 0,
+                    eax: 0,
+                    ecx: 0,
+                    edx: 0,
+                    ebx: 0,
+                    esp: 0,
+                    ebp: 0,
+                    esi: 0,
+                    edi: 0,
+                    es: USER_DS.as_u32(),
+                    cs: USER_CS.as_u32(),
+                    ss: USER_DS.as_u32(),
+                    ds: USER_DS.as_u32(),
+                    fs: USER_DS.as_u32(),
+                    gs: USER_DS.as_u32(),
+                    ldt: selectors::ldt_selector(0).as_u32(),
+                    trace_bitmap: 0x8000_0000,
+                    i387: I387Struct::empty(),
+                },
+            },
         ));
 
         // Create Task from the static address.
